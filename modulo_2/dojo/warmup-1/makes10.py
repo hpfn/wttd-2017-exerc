149bae3ce529dbd8f0c9065fc8e3ec4330fecc65
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-06-04 10:44:05 -0300

    script and tests ok.

---
+++ modulo_2/dojo/warmup-1/makes10.py
@@ -0,0 +1,3 @@
+# coding=utf-8
+def makes10(int_x, int_y):
+    return (int_x == 10 or int_y == 10) or ((int_x + int_y) == 10)
---
+++ modulo_2/dojo/warmup-1/test_makes10.py
@@ -0,0 +1,13 @@
+import unittest
+from makes10 import makes10
+
+
+class Makes10Test(unittest.TestCase):
+    def test_10_true(self):
+        self.assertTrue(makes10(9, 10))
+
+    def test_sum_10_false(self):
+        self.assertFalse(makes10(9, 9))
+
+    def test_sum_10_true(self):
+        self.assertTrue(makes10(1, 9))
