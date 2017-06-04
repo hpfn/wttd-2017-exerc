9a7b95e73fd86a95a1901e811f1b1022ce234501
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-06-04 10:16:46 -0300

    script and test ok

---
+++ modulo_2/dojo/warmup-1/parrot_trouble.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+def parrot_trouble(talking, hour):
+    if (hour < 7 or hour > 20) and talking:
+        return True
+
+    return False
---
+++ modulo_2/dojo/warmup-1/test_parrot_trouble.py
@@ -0,0 +1,28 @@
+import unittest
+from parrot_trouble import parrot_trouble
+
+
+class ParrotTroubleTest(unittest.TestCase):
+    def test_trouble_in_6_talk(self):
+        self.assertTrue(parrot_trouble(True, 6))
+
+    def test_trouble_in_6_not_talk(self):
+        self.assertFalse(parrot_trouble(False, 6))
+
+    def test_trouble_in_7_talk(self):
+        self.assertFalse(parrot_trouble(True, 7))
+
+    def test_trouble_in_7_not_talk(self):
+        self.assertFalse(parrot_trouble(False, 7))
+
+    def test_trouble_in_20_talk(self):
+        self.assertFalse(parrot_trouble(True, 20))
+
+    def test_trouble_in_20_not_talk(self):
+        self.assertFalse(parrot_trouble(False, 20))
+
+    def test_trouble_in_21_talk(self):
+        self.assertTrue(parrot_trouble(True, 21))
+
+    def test_trouble_in_21_not_talk(self):
+        self.assertFalse(parrot_trouble(False, 21))
