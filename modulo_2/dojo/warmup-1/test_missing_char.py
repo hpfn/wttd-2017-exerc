2134904032fc9be502980e4d465056d75af34637
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-06-07 16:44:55 -0300

    equal instead of true

--- modulo_2/dojo/warmup-1/test_missing_char.py
+++ modulo_2/dojo/warmup-1/test_missing_char.py
@@ -4,10 +4,10 @@ from missing_char import missing_char
 
 class MissingCharTest(unittest.TestCase):
     def test_missing_char_0(self):
-        self.assertTrue(missing_char('kitten', 0),'itten')
+        self.assertEqual(missing_char('kitten', 0),'itten')
 
     def test_missing_char_1(self):
-        self.assertTrue(missing_char('kitten', 1),'ktten')
+        self.assertEqual(missing_char('kitten', 1),'ktten')
 
     def test_missing_char_4(self):
-        self.assertTrue(missing_char('kitten', 4),'kittn')
+        self.assertEqual(missing_char('kitten', 4),'kittn')
