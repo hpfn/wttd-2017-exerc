b15f2978d5f34bf2de161dc411e39f96eacb8f2b
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-06-09 13:54:04 -0300

    improvement

--- modulo_2/dojo/warmup-1/pos_neg.py
+++ modulo_2/dojo/warmup-1/pos_neg.py
@@ -1,12 +1,12 @@
+# coding=utf-8
 def pos_neg(a, b, negative):
-    #if not negative and (a > 0 and b < 0):
-    #    return True
     if not negative:
-        if (a > 0 and b < 0) or (a < 0 and b > 0):
-            return True
+        return (a > 0 > b) or (a < 0 < b)
+        # if (a > 0 and b < 0) or (a < 0 and b > 0):
+        #    return True
     else:
-        if (a < 0 and b < 0):
-            return True
+        return a < 0 and b < 0
+        # if (a < 0 and b < 0):
+        #    return True
 
-
-    return False
+    # return False
