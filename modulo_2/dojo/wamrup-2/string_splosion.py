895cd95040ee51c12cadc92ce40bc52345e0edbd
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-06-19 14:30:00 -0300

    script improvement

--- modulo_2/dojo/wamrup-2/string_splosion.py
+++ modulo_2/dojo/wamrup-2/string_splosion.py
@@ -1,7 +1,9 @@
 # coding=utf-8
 def string_splosion(str):
-    l = str[0]
-    for x in range(2, len(str)+1):
-        l = l + str[0] + str[1:x]
-
+    #l = str[0]
+    #for x in range(2, len(str)+1):
+    #    l = l + str[0] + str[1:x]
+    l = ''
+    for x in range(len(str)):
+        l += str[:x+1]
     return l
