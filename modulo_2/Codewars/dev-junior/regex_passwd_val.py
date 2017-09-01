f5cf64831d8d242f6f6ffbbc3c042cd8fa9636fd
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-09-01 14:36:07 -0300

    script ok. '\s' is not necessary.

--- modulo_2/Codewars/dev-junior/regex_passwd_val.py
+++ modulo_2/Codewars/dev-junior/regex_passwd_val.py
@@ -2,4 +2,4 @@
 from re import search, fullmatch
 
 def regex_passwd_val(str_pass):
-    return search(r'^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?!.*[\W\s_]).{6,}$', str_pass)
+    return search(r'^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?!.*[\W_]).{6,}$', str_pass)
